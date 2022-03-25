6c19da15900bae9f3aca3eac27e7cac998ebe91f
Ryotaro Hada <ryotarohada@gmail.com>
2022-03-25 23:01:55 +0900

    Delete Context

--- src/context/index.tsx
+++
@@ -1,7 +0,0 @@
-export namespace Contexts {
-  export type Theme = 'light' | 'dark' | 'prefer'
-}
-
-// アプリケーションのテーマを設定
-// ここでのテーマとはユーザーの操作結果を伴わない初期状態のことを指す
-export const AppTheme: Contexts.Theme = 'dark'
